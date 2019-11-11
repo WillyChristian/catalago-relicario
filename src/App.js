06c35d7f35b576fa49456943e1170727a2535e39
WillyChristian <crisort@gmail.com>
2019-11-11 17:45:02 -0300

    Aplicado função para exibir pdf

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import LogoEscrito from './Component/IMG/Logo/LogoEscrito.png'
 import './App.css';
+import {Document, Page} from 'react-pdf'
+import catalogo from './Component/Catalogo/catalogo.pdf'
+import { pdfjs } from 'react-pdf';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
-function App() {
+
+class App extends Component {
+
+  state = {
+    numPages : null,
+    pageNumber: 1
+  }
+
+  onDocumentLoadSuccess = ({ numPages }) => {
+    this.setState({numPages})
+  }
+
+
+render(){ 
+  const {pageNumber, numPages} = this.state
+  
   return (
     <div className="App">
         
@@ -14,23 +33,31 @@ function App() {
 
           <span className="info-page">
             Página
-              <span className="page-num"></span>
+              <span className="page-num">{pageNumber}</span>
             de
-              <span className="page-cont"></span>
+              <span className="page-cont">{numPages}</span>
           </span>
         <button className="btn" id="next-page">Próximo
                 <i className="fas fa-arrow-circle-right m-1"></i>
         </button>
         </section>
 
-        <section >
-         
-        <div className="container d-none">
+        <section className="container">
+          <Document file={catalogo} 
+                    onLoadSuccess={this.onDocumentLoadSuccess}
+                     >
+                       
+            <Page pageNumber={pageNumber} />
+          </Document>
 
-          </div>
         </section>
+
+
+
+
     </div>
   );
+  }
 }
 
 export default App;
