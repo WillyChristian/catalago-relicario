b3c09b2ebe9c0981c760b6087334aa79722ac0ac
WillyChristian <crisort@gmail.com>
2019-11-13 12:18:59 -0300

    Add motion to test gsap

---
+++ src/Component/Header/animaLogo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Tween, Timeline} from 'react-gsap'
+
+const TimelineComponent = () => (
+    <Timeline duration="2">
+        <Tween from={{ opacity: 0 }} to={{ opacity: 1 }} />
+        <Tween from={{ x: '-50px' }} to={{ x: '0px' }} />
+    </Timeline>
+
+)
+
+export default TimelineComponent
--- src/Component/Header/index.js
+++ src/Component/Header/index.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react'
 import LogoEscrito from '../IMG/Logo/LogoEscrito.png'
-
 import './style.css'
 
+// tentativa de animação com react-gsap
+// import TimelineComponent from "./animaLogo"
+// import { Timeline, Tween } from "react-gsap";
+
+// animeLogo = () => {
+//     const anime = this.TimelineComponent  
+// }
+
+
+// TESTE COM O GSAP ORIGINAL
+import gsap from 'gsap'
+
 export default class Header extends Component {
+
+animaImagem = () =>{
+    
+    gsap.to(".logo", {duration: 1, x:150, rotation: 135})
+    // console.log('teste de click')
+}
     render() {
+
         return (
             <div className="header">
                 <nav className="nav-header">
-                       <button className="btn menu-toggle"><i class="fas fa-bars 5x"></i></button>
-                       <img src={LogoEscrito} alt="logoEscrito"/>
+                       <button className="btn menu-toggle" onClick={this.animaImagem}><i className="fas fa-bars 5x"></i></button>
+                       <img src={LogoEscrito} alt="logoEscrito" className="logo"/>
                 </nav>
             </div>
         )
