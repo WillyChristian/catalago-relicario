1420dcde057032e68e78b95f86bd1457efa7d76e
WillyChristian <crisort@gmail.com>
2019-11-14 17:27:42 -0300

    Some layout ajustment

--- src/App.js
+++ src/App.js
@@ -10,18 +10,14 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div className="App">
-          <section className="header">
-            <Header /> {/* aqui ta o botão sandwich */}
-          </section>
-          <Switch>
-            {/****************** BOTÃO MENU E LOGO ************************/}
-            <Route path="/" exact component={Home} />
-            <Route path="/bf-promo" component={Catalogo} />
-            <Route path="/cat-moveis" component={Moveis} />
-            <Route path="/cat-estofados" component={Estofados} />
-          </Switch>
-        </div>
+            <Header /> 
+            <Switch>
+              {/****************** BOTÃO MENU E LOGO ************************/}
+              <Route path="/" exact component={Home} />
+              <Route path="/bf-promo" component={Catalogo} />
+              <Route path="/cat-moveis" component={Moveis} />
+              <Route path="/cat-estofados" component={Estofados} />
+            </Switch>
       </Router>
     );
   }
--- src/Component/Home.js
+++ src/Component/Home.js
@@ -4,7 +4,7 @@ export default class Home extends Component {
     render() {
         return (
             <div>
-                
+                <h1>Hello World .. i'm in the Home</h1>
             </div>
         )
     }
