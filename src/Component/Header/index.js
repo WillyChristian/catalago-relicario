2a4220cd1f58eea656e5d2474edd07f980c928bb
WillyChristian <crisaort@gmail.com>
2019-11-15 03:36:44 -0300

    Add meny motions (has to be aproved)

--- src/Component/Header/index.js
+++ src/Component/Header/index.js
@@ -1,34 +1,90 @@
 import React, { Component } from "react";
 import LogoEscrito from "../IMG/Logo/LogoEscrito.png";
 import "./style.css";
-import Menu from "../Menu/index";
+import { Link } from "react-router-dom";
+import gsap from "gsap";
 
 export default class Header extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      abreMenu: "0"
-    };
-    this.abreMenu = this.abreMenu.bind(this);
-  }
-
   abreMenu = () => {
-    if (this.state.abreMenu === "0") {
-      this.setState({ abreMenu: "1" });
+    let x = document.querySelector(".menu");
+    if (x.id !== "abre") {
+      x.setAttribute("id", "abre");
+      this.animaMenu();
+      console.log("abriu o menu")
     } else {
-      this.setState({ abreMenu: "0" });
+      x.setAttribute("id", "fecha");
+      this.animaReverse();
+      console.log("Chamou a animação e o ID ta setado ó: "+ x.id)
     }
   };
+
+  animaMenu = () => {
+    console.log("abriu a animação")
+    let tl = gsap.timeline();
+    tl.from(".link0", { duration: 0.9, x: -300 })
+      .from(".link1", { duration: 0.8, x: -300 }, "-=.7")
+      .from(".link2", { duration: 0.7, x: -300 }, "-=.6")
+      .from(".link3", { duration: 0.6, x: -300 }, "-=.5")
+      .from(".empty", { duration: 0.5, x: -300 }, "-=.4")
+      .from(".empty1", { duration: 0.5, x: -300 }, "-=.3")
+      .from(".empty2", { duration: 0.5, x: -300 }, "-=.2");
+      console.log("acabou a animação")
+  };
+
+  animaReverse = () => {
+    console.log("abriu a animação")
+
+    let tl = gsap.timeline();
+    tl.to(".empty2", { duration: 0.9, x: -300 })
+    //   .to(".empty1", { duration: 0.8, x: -300 }, "-=.7")
+    //   .to(".empty", { duration: 0.7, x: -300 }, "-=.6")
+    //   .to(".link3", { duration: 0.6, x: -300 }, "-=.5")
+    //   .to(".link2", { duration: 0.5, x: -300 }, "-=.4")
+    //   .to(".link1", { duration: 0.5, x: -300 }, "-=.3")
+    //   .to(".link0", { duration: 0.5, x: -300 }, "-=.2");
+    //   console.log("acabou a animação")
+
+  };
+
   render() {
     return (
-      <div className="header">
-        <nav className="nav-header">
-          <button className="btn menu-toggle" onClick={this.abreMenu}>
-            <i className="fas fa-bars 5x"></i>
-          </button>
-          <img src={LogoEscrito} alt="logoEscrito" className="logo" />
-        </nav>
-        {this.state.abreMenu === "1" ? <Menu /> : null}
+      <div>
+        <div className="header">
+          <nav className="nav-header">
+            <button className="btn menu-toggle" onClick={this.abreMenu}>
+              <i className="fas fa-bars 5x"></i>
+            </button>
+            <img src={LogoEscrito} alt="logoEscrito" className="logo" />
+          </nav>
+        </div>
+
+        <div className="menu" id="fecha">
+          <div className="hide-menu">
+            <div className="menu-row link0">
+              <Link to="/bf-promo">
+                <li className="nav-link">Promoção Black Friday</li>
+              </Link>
+            </div>
+            <div className="menu-row link1">
+              <Link to="/cat-moveis">
+                <li className="nav-link">Catálogo de Móveis </li>
+              </Link>
+            </div>
+            <div className="menu-row link2">
+              <Link to="/cat-estofados">
+                <li className="nav-link">Catálogo Estofados</li>
+              </Link>
+            </div>
+            <div className="menu-row link3">
+              <Link to="/">
+                <li className="nav-link">Voltar para o Menu</li>
+              </Link>
+            </div>
+            <div className="empty"></div>
+            <div className="empty1"></div>
+            <div className="empty2"></div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/Component/Home.js
+++ src/Component/Home.js
@@ -5,7 +5,6 @@ import "./style.css";
 
 export default class Home extends Component {
   animaLogo = () => {
-    // gsap.from(".logo-home", )
     let tl = gsap.timeline();
     tl.from(".logo-home", {
       delay: 0.5,
@@ -13,9 +12,28 @@ export default class Home extends Component {
       height: 100,
       opacity: 0
     })
-      .to(".logo-home", { y: -290, duration: 2, opacity: 0, height: 38 })
-      .to("logo-container", { duration: 1, opacity: "0" });
+      .to(".logo-home", { y: -290, duration: 1, opacity: 0, height: 38 })
+      .to("logo-container", { duration: 1, opacity: "0" })
+      .from("txt", { opacity: 0, y: -50, onComplete: this.mostraTexto });
   };
+
+  mostraTexto = () => {
+    let x = document.querySelector(".h1");
+    let y = document.querySelector(".p");
+    if (x.id !== "mostra" && y.id !== "mostra") {
+      x.setAttribute("id", "mostra");
+      y.setAttribute("id", "mostra");
+      console.log('funcionou até mudar o ID')
+      this.animaTexto();
+    }
+  };
+
+  animaTexto = () => {
+    let tl = gsap.timeline();
+      tl.from(".h1", { duration: 1, yx: -50 })
+      .from(".p", { duration: 1, y: -10 }, "-=.4");
+  };
+
   render() {
     return (
       <div>
@@ -27,6 +45,10 @@ export default class Home extends Component {
             onLoad={this.animaLogo}
           />
         </div>
+        <h1 id="i" className="txt h1">Seja Bem vindo(a)!</h1>
+        <p id="i" className="txt p">
+          Utilize o menu acima para navegar entre os diferentes catálogos
+        </p>
       </div>
     );
   }
