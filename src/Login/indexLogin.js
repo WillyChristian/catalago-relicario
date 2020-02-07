f128122c9f6f439d950e5a445e6e940d62905154
WillyChristian <crisort@gmail.com>
2020-02-07 16:01:18 -0300

    Switch from Yarn to NPM

--- src/Login/indexLogin.js
+++ src/Login/indexLogin.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
 import "./styleLogin.css"
+import axios from 'axios'
+
 export default class indexLogin extends Component {
+
+login = () =>{
+    axios.post(
+        "http://localhost:5000/user/auth"
+    ).then( response => {
+        let message = document.getElementById("msg")
+        if(response.status === 200){
+            message.setAttribute("value", "Usuário logado com sucesso")
+        }
+    } )
+}
     render() {
         return (
             <div className="login">
                 <h1>Login</h1>
-                <form action="POST" className="login-form">
+                <span className="container" id="msg"></span>
+                <form className="login-form">
                     <div className="data">
                         <label htmlFor="username">Email</label>               
                         <input type="email" name="username" id="username" />                                  
@@ -14,8 +28,8 @@ export default class indexLogin extends Component {
                         <label htmlFor="password"> Senha</label>
                         <input type="password" name="password" id="password"/>
                     </div>
+                <input className="btn btn-success btn-lg|sm" type="button" value="Logar" onClick={this.login} />
                 </form>
-                <input type="submit" className="btn btn-success"/>
                 <div className="create-account">
                     Não possui cadastro? <a href="localhost:3000">Cadastr-se!</a>
                 </div>
